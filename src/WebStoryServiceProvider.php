36cb8b4d48e21a7ffb97603f1cb75ada48f07395
Ashok Devatwal <ashokdevatwal@tractorjunction.com>
2022-12-31 16:43:33 +0530

    fix

--- src/WebStoryServiceProvider.php
+++ src/WebStoryServiceProvider.php
@@ -7,7 +7,7 @@ class WebStoryServiceProvider extends ServiceProvider {
     
     public function boot()
     {
-
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
     
     public function register()
