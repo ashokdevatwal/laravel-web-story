6b923b7b976eb1bff9eaacf549427f4447f7b9b9
Ashok Devatwal <ashokdevatwal@tractorjunction.com>
2023-08-18 18:08:46 +0530

    done

---
+++ src/Models/WebStoryAnimation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Devatwal\LaravelWebStory\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WebStoryAnimation extends Model
+{
+    protected $fillable = ['name', 'value'];
+}
--- src/database/migrations/2022_12_31_112148_create_web_story_animations_table.php
+++ src/database/migrations/2022_12_31_112148_create_web_story_animations_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+
+use Devatwal\LaravelWebStory\Models\WebStoryAnimation;
 
 class CreateWebStoryAnimationsTable extends Migration
 {
@@ -19,6 +22,37 @@ class CreateWebStoryAnimationsTable extends Migration
             $table->string('value', 50);
             $table->timestamps();
         });
+
+        $animations = [
+            [
+                'name' => 'Fly In Left',
+                'value' => 'fly-in-left'
+            ],
+            [
+                'name' => 'Fade In',
+                'value' => 'fade-in'
+            ],
+            [
+                'name' => 'Twirl In',
+                'value' => 'twirl-in'
+            ],
+            ['name' => 'Fly In Right',      'value' => 'fly-in-right'],
+            ['name' => 'Fly In Top',        'value' => 'fly-in-top'],
+            ['name' => 'Fly In Bottom',     'value' => 'fly-in-bottom'],
+            ['name' => 'Rotate In Left',    'value' => 'rotate-in-left'],
+            ['name' => 'Rotate In Right',   'value' => 'rotate-in-right'],
+            ['name' => 'Drop In',           'value' => 'drop'],
+            ['name' => 'Whoosh In Left',    'value' => 'whoosh-in-left'],
+            ['name' => 'Whoosh In Right',   'value' => 'whoosh-in-right'],
+            ['name' => 'Zoom In',           'value' => 'zoom-in'],
+            ['name' => 'Zoom Out',          'value' => 'zoom-out'],
+            ['name' => 'Pan Left',          'value' => 'pan-left'],
+            ['name' => 'Pan Right',         'value' => 'pan-right'],
+            ['name' => 'Pan Up',            'value' => 'pan-up'],
+            ['name' => 'Pan Down',          'value' => 'pan-down']
+        ];
+
+        collect($animations)->each(function ($animation) { WebStoryAnimation::create($animation); });
     }
 
     /**
